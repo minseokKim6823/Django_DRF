636dc9054ae0783f9c1570f4338507524aeeeabe
minseokKim6823 <alstjr6823@naver.com>
2023-06-24 01:38:04 +0900

    API

--- myweb/example/views.py
+++ myweb/example/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+@api_view(['GET'])
+def HelloAPI(request):
+    return Response("hello world!")
